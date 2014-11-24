d4843420a8a2935e58ba38202b2e9cd14f41d3f8
Tibor Udvari <tibor.udvari@icloud.com>
2014-11-24 20:36:07 +0100

    Row logic

---
+++ MarketMayhem/Assets/Scripts/SpawnLogic.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnLogic : MonoBehaviour {
+
+	private Vector3 spawnPoint;
+	public GameObject cubeC;
+	//public List<GameObject> personList;
+	private GameObject personClone;
+
+	public int rowNumber = 5;
+	private float xPos;
+	private float yPos;
+	private float bottomPadding = 40;
+	private float topPadding = 40;
+	public List<float> ySpawnPosList = new List<float>();
+
+
+	void Start () {
+
+		PopulateSpawnPosition ();
+	}
+	
+
+	void FixedUpdate () {
+	
+	}
+
+	void PopulateSpawnPosition() {
+		float step = (Screen.height - bottomPadding - topPadding) / rowNumber;
+		for (int i = 0; i < rowNumber; i++) {
+			float yPos = bottomPadding + i * step + step/2;
+			ySpawnPosList.Add(yPos);
+			spawnPoint = Camera.main.ScreenToWorldPoint (new Vector3(Screen.width * 0.5f, yPos, 10));
+			personClone = Instantiate (cubeC, spawnPoint, Quaternion.identity) as GameObject;
+		}
+		Debug.Log (ySpawnPosList.Count);
+	}
+}
