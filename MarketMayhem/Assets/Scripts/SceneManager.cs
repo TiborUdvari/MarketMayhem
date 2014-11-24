c065b930a7ad7f28e073a56311899449969afea3
Tibor Udvari <tibor.udvari@icloud.com>
2014-11-24 19:26:13 +0100

    Scenes layout

---
+++ MarketMayhem/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Scenestate
+{
+	LOAD,
+	START,
+	GAME,
+	END
+}
+
+
+public class SceneManager : MonoBehaviour {
+
+
+	public Scenestate actualState;
+	public bool isStarting = false; 
+
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	void Start () {
+		actualState = Scenestate.LOAD;
+	}
+
+	void Update () {
+		ChangeState();
+
+	}
+
+	private void ChangeState() 
+	{
+		switch (actualState) 
+		{
+		case Scenestate.LOAD:
+			Application.LoadLevel(1);
+			actualState = Scenestate.START;
+			isStarting = true;
+			break;
+		case Scenestate.START:
+			StStart();
+			break;
+		case Scenestate.GAME:
+			StIngameLoop();
+			break;
+		case Scenestate.END:
+			StEndscreen();
+			break;
+		}
+
+	}
+		
+	void OnTouchDown() {
+		Debug.Log ("start game");
+		actualState = Scenestate.GAME;
+		
+	}
+
+	public void StStart() {
+
+
+	}
+
+	public void StIngameLoop() {
+
+
+
+
+
+
+		//if game over
+		//actualState = Scenestate.END;
+	}
+
+	public void StEndscreen() {
+
+		//display High-score
+	//	if (Input.GetTouch) {
+			Debug.Log ("start new game");
+			actualState = Scenestate.GAME;
+			isStarting = false;
+	//	}
+	}
+
+
+}
---
+++ MarketMayhem/Assets/Scripts/StartGameButton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGameButton : MonoBehaviour {
+
+	public Color defaultColor;
+	public Color selectedColor;
+	private Material mat;
+
+	void Start() {
+		mat = renderer.material;
+	}
+
+	void OnTouchDown() {
+		mat.color = selectedColor;
+		if (Application.loadedLevel == 1) {
+			Application.LoadLevel(2);
+		} else if (Application.loadedLevel == 3) {
+			Application.LoadLevel(2);
+		}
+	}
+
+	void OnTouchUp() {
+		mat.color = defaultColor;
+	}
+
+	void OnTouchStay() {
+		mat.color = selectedColor;
+	}
+
+	void OnTouchExit() {
+		mat.color = defaultColor;
+	}
+
+}
