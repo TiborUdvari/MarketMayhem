4c480d5565f55f4d4a349fa2352096e76eafb86f
Tibor Udvari <tibor.udvari@icloud.com>
2014-11-25 09:41:33 +0100

    Spawning from the left side

---
+++ MarketMayhem/Assets/Scripts/PersonController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum CharacterState
+{
+	WALKING,
+	WATCHING,
+	INTERACTING,
+	PISSED
+}
+
+public class PersonController : MonoBehaviour {
+
+	// Attributes 
+	public CharacterState state; // TODO setter
+	public float speed;
+	public float direction;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		direction = 1.0f;
+		speed = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		rigidbody2D.MovePosition (new Vector3 ( rigidbody2D.transform.position.x + speed * direction * Time.deltaTime  , rigidbody2D.transform.position.y, 0.0f));
+		HandleCleanup ();
+	
+	}
+
+	void HandleCleanup()
+	{
+		float padding = 0.0f;
+		Vector3 exitPosition = Camera.main.WorldToScreenPoint ( new Vector3(rigidbody2D.transform.position.x + padding, 0.0f, 0.0f));
+		if (exitPosition.x >= Screen.width)
+		{
+			Debug.Log("DESTROOOOOOOOOOOY BLEAAAH BLOOOD ");
+			Destroy(gameObject);
+		}
+	}
+}
--- MarketMayhem/Assets/Scripts/SpawnLogic.cs
+++ MarketMayhem/Assets/Scripts/SpawnLogic.cs
@@ -1,39 +1,75 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 
 public class SpawnLogic : MonoBehaviour {
 
 	private Vector3 spawnPoint;
-	public GameObject cubeC;
+	public GameObject person;
 	//public List<GameObject> personList;
 	private GameObject personClone;
 
-	public int rowNumber = 5;
+	private int rowNumber = 4;
 	private float xPos;
 	private float yPos;
-	private float bottomPadding = 40;
-	private float topPadding = 40;
+	private float bottomPadding = 20;
+	private float topPadding = 20;
 	public List<float> ySpawnPosList = new List<float>();
+	private Timer timer;
 
 
-	void Start () {
-
+	// --- Hacks --- 
+	int timerHack;
+	
+	void Start () 
+	{
 		PopulateSpawnPosition ();
-	}
-	
-
-	void FixedUpdate () {
-	
+		//timer = new System.Threading.Timer(obj => { SpawnPerson(); }, null, 1000, System.Threading.Timeout.Infinite);
+		//StartCoroutine (SpawnPerson());
+		StartCoroutine (spawn());
 	}
 
-	void PopulateSpawnPosition() {
+
+	IEnumerator spawn()
+	{
+		while (true) {
+			yield return new WaitForSeconds (1);
+			SpawnPerson();
+		}
+	}
+
+	public void SpawnPerson()
+	{
+		Debug.Log ("Spawn person. ");
+
+		int yPosIndex = Random.Range (0, ySpawnPosList.Count);
+		float yPos = ySpawnPosList[yPosIndex];
+
+		float padding = 0; // TODO get half of sprite size
+		bool side = yPos % 2 == 0;
+
+		float xPos = side ? -padding : -padding;
+
+		Vector3 initPosition =  Camera.main.ScreenToWorldPoint( new Vector3 ( xPos, yPos, 9.0f));
+		personClone = Instantiate (person, initPosition, Quaternion.identity) as GameObject;
+	}
+
+	void FixedUpdate () 
+	{
+
+	}
+	
+	// --- Person generation ---
+
+	void PopulateSpawnPosition() 
+	{
 		float step = (Screen.height - bottomPadding - topPadding) / rowNumber;
 		for (int i = 0; i < rowNumber; i++) {
 			float yPos = bottomPadding + i * step + step/2;
 			ySpawnPosList.Add(yPos);
 			spawnPoint = Camera.main.ScreenToWorldPoint (new Vector3(Screen.width * 0.5f, yPos, 10));
-			personClone = Instantiate (cubeC, spawnPoint, Quaternion.identity) as GameObject;
+
 		}
 		Debug.Log (ySpawnPosList.Count);
 	}
